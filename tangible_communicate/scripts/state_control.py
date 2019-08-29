77d607db5004eb28d4d834f50c9611b7d4e5531d
Patrick <patrick_hoey1@student.uml.edu>
2019-08-29 16:18:59 -0400

    Refractored everything so it was class based. added majority of scooter stuff to it. should be able to succesfully pick up an object now

--- tangible_communicate/scripts/state_control.py
+++ tangible_communicate/scripts/state_control.py
@@ -3,150 +3,158 @@
 import rospy
 from tangible_master.msg import button_leds
 
-def set_state(state_name, recieved_msg, pick_cloud_gathered, basket_complete, place_cloud_gathered, placed_complete):
+def set_state(tangible):
     """
     Handles the state of the tangible user interface based upon the input recieved via the buttons and current system
     status
     buttons go in this order [yes, no, pick, place, basket, drive]
     leds go in this order [thinking, wait for input, wrist lasers]
     """
-    if(state_name == "driving"):
-        if(recieved_msg.button == [0, 0, 1, 0, 0, 0]):
-            state_name = "gather_pick_cloud"
-        else:
-            pass
+    if(tangible.state == "driving"):
+        if(tangible.recieved_msg.button[2] == 1):
+            tangible.state = "gather_pick_cloud"
 
     #TODO add is pointcloud gathered flag
-    if(state_name == "gather_pick_cloud"):
-        if(pick_cloud_gathered):
-            state_name == "pick"
-        else:
-            pass
+    if(tangible.state == "gather_pick_cloud"):
+        if(tangible.pick_cloud_gathered):
+            tangible.state == "pick"
+            tangible.pick_cloud_gathered = False
 
-    if(state_name == "pick"):
-        if(recieved_msg.button == [0, 0, 0, 0, 0, 1]):
-            state_name = "driving"
-        elif(recieved_msg.button == [1, 0, 0, 0, 0, 0]):
-            state_name = "confirm_object"
-        else:
-            pass
+    if(tangible.state == "pick"):
+        if(tangible.recieved_msg.button[5] == 1):
+            tangible.state = "driving"
+        elif(tangible.recieved_msg.button[0] == 1):
+            tangible.state = "confirm_object"
 
-    if(state_name == "confirm_object"):
-        if(recieved_msg.button == [1, 0, 0, 0, 0, 0]):
-            state_name = "confirm_pick"
-        elif(recieved_msg.button == [0, 1, 0, 0, 0, 0]):
+    #TODO IMPLEMENT METHOD OF ONLY TAKING IN MESSAGE ONCE, WHETER THAT BE A REST OR SOMETHING ELSE
+    if(tangible.state == "confirm_object"):
+        if(tangible.recieved_msg.button[0] == 1):
+            tangible.state = "confirm_pick"
+        elif(tangible.recieved_msg.button[1] == 1):
             state_name = "driving"
-        else:
-            pass
 
     #Not sure how this is working with the grasp sucess detection code
-    if(state_name == "confirm_pick"):
-        if(recieved_msg == [1, 0, 0, 0, 0, 0]):
-            state_name = "object_held"
-        elif(recieved_msg == [1, 0, 0, 0, 0, 0]):
-            state_name = "driving"
-        else:
-            pass
+    if(tangible.state == "confirm_pick"):
+        if(tangible.recieved_msg[1] == 1):
+            tangible.state = "object_held"
+        elif(tangible.recieved_msg[1] == 1):
+            tangible.state = "driving"
 
     if(state_name == "object_held"):
-        if(recieved_msg == [0, 0, 0, 0, 0, 1]):
-            state_name = "driving"
-        elif(recieved_msg == [0, 0, 0, 1, 0, 0]):
-            state_name = "gather_place_cloud"
-        elif(recieved_msg == [0, 0, 0, 0, 1, 0]):
-            state_name = "basket"
-        else:
-            pass
+        if(tangible.recieved_msg[5] == 1):
+            tangible.state = "driving"
+        elif(tangible.recieved_msg[3] == 1):
+            tangible.state = "gather_place_cloud"
+        elif(tangible.recieved_msg[4] == 1):
+            tangible.state = "basket"
 
     #TODO Need complete flag
-    if(state_name == "basket"):
-        if(basket_complete):
-            state_name = "driving"
-        else:
-            pass
+    if(tangible.state == "basket"):
+        if(tangible.basket_complete):
+            tangible.state = "driving"
+            tangible.basket_complete = False
+
     #TODO need cloudReady flag
-    if(state_name == "gather_place_cloud"):
-        if(place_cloud_gathered):
-            state_name = "place"
-        else:
-            pass
+    if(tangible.state == "gather_place_cloud"):
+        if(tangible.place_cloud_gathered):
+            tangible.state = "place"
+            tangible.place_cloud_gathered = False
 
-    if(state_name == "place"):
-        if(recieved_msg == [1, 0, 0, 0, 0, 0]):
-            state_name = "place_confirm"
-        else:
-            pass
+    if(tangible.state == "place"):
+        if(tangible.recieved_msg[0] == 1):
+            tangible.state = "place_confirm"
 
-    if(state_name == "place_confirm"):
-        if(recieved_msg == [1, 0, 0, 0, 0, 0]):
-            state_name = "placing"
-        elif(recieved_msg == [0, 1, 0, 0, 0, 0]):
-            state_name = "place"
-        else:
-            pass
+    if(tangible.state == "place_confirm"):
+        if(tangible.recieved_msg[0] == 1):
+            tangible.state = "placing"
+        elif(tangible.recieved_msg[1] == 1):
+            tangible.state = "place"
 
     #TODO Need placed confirm flag
-    if(state_name == "placing" and placed_complete):
-        state_name = "driving"
+    if(tangible.state == "placing" and tangible.placed_complete):
+        tangible.state = "driving"
+        tangible.placed_complete = False
 
 
 
 
 
-def compare_state(state_name, button_msg):
+def compare_state(tangible):
     """
     Compares current state to desired LED state for the interface
     """
+    #Buttons[yes, no, pick, place, basket, drive]
+    #Leds
     #1 means solid light on, 2 means that it should be blinking, 0 is off
     #first led is thinking, second is ready for selection
-    if(state_name == "driving"):
-        button_msg.button = [0, 0, 2, 0, 0, 1]
-        button_msg.leds = [0, 0, 0]
+    if(tangible.state == "driving"):
+        tangible.button_msg.button = [0, 0, 2, 0, 0, 1]
+        tangible.button_msg.leds = [0, 0, 0]
 
-    elif(state_name == "gather_pick_cloud"):
-        button_msg.button = [0, 0, 1, 0, 0, 0]
-        button_msg.leds = [1, 0, 0]
+        tangible.scooter.go_to_travel_config()
 
-    elif(state_name == "pick"):
-        button_msg.button = [2, 2, 1, 0, 0, 0]
-        button_msg.leds = [0, 1, 1]
-
-    elif(state_name == "confirm_object"):
-        button_msg.button = [2, 2, 1, 0, 0, 0]
-        button_msg.leds = [0, 0, 0]
-
-    elif(state_name == "confirm_pick"):
-        button_msg.button = [2, 2, 0, 0, 0, 0]
-        button_msg.leds = [0, 0, 0]
+    elif(tangible.state == "gather_pick_cloud"):
+        tangible.button_msg.button = [0, 0, 1, 0, 0, 0]
+        tangible.button_msg.leds = [1, 0, 0]
 
 
-    elif(state_name == "object_held"):
-        button_msg.button = [0, 0, 0, 2, 2, 1]
-        button_msg.leds = [0, 0, 0]
+
+        tangible.scooter.go_to_fold_config()
+        tangible.scooter.update_pointcloud()
+
+    elif(tangible.state == "pick"):
+        tangible.button_msg.button = [2, 2, 1, 0, 0, 0]
+        tangible.button_msg.leds = [0, 1, 1]
+
+        tangible.center = tangible.scooter.wait_for_laser()
+        tangible.sample_points = tangible.scooter(tangible.scooter.get_sample_points(tangible.center))
+
+        if tangible.sample_points is None or len(tangible.sample_points) == 0:
+            tangible.sample_points = tangible.scooter.get_sample_points(tangible.center)
 
 
-    elif(state_name == "gather_place_cloud"):
-        button_msg.button = [0, 0, 0, 1, 0, 0]
-        button_msg.leds = [1, 0, 0]
+    elif(tangible.state == "confirm_object"):
+        tangible.button_msg.button = [2, 2, 1, 0, 0, 0]
+        tangible.button_msg.leds = [0, 0, 0]
+
+    elif(tangible.state == "currently_picking"):
+        tangible.grasp  = tangible.scooter.get_reachable_grasps(tangible.sample_points)
+        tangible.grasp = tangible.scooter.automatic_grasp_selection(tangible.grasp, tangible.sample_points, tangible.center)
+        tangible.scooter.pick_object(tangible.grasp)
 
 
-    elif(state_name == "place"):
-        button_msg.button = [2, 2, 0, 1, 0, 0]
-        button_msg.leds = [0, 1, 1]
+    elif(tangible.state == "confirm_pick"):
+        tangible.button_msg.button = [2, 2, 0, 0, 0, 0]
+        tangible.button_msg.leds = [0, 0, 0]
 
 
-    elif(state_name == "place_confirm"):
-        button_msg.button = [2, 2, 0, 1, 0, 0]
-        button_msg.leds = [0, 0, 0]
+    elif(tangible.state == "object_held"):
+        tangible.button_msg.button = [0, 0, 0, 2, 2, 1]
+        tangible.button_msg.leds = [0, 0, 0]
 
 
-    elif(state_name == "basket"):
-        button_msg.button = [0, 0, 0, 0, 1, 0]
-        button_msg.leds = [0, 0, 0]
+    elif(tangible.state == "gather_place_cloud"):
+        tangible.button_msg.button = [0, 0, 0, 1, 0, 0]
+        tangible.button_msg.leds = [1, 0, 0]
 
 
-    elif(state_name == "placing"):
-        button_msg.button = [1, 0, 0, 1, 0, 0]
-        button_msg.leds = [0, 0, 1]
+    elif(tangible.state == "place"):
+        tangible.button_msg.button = [2, 2, 0, 1, 0, 0]
+        tangible.button_msg.leds = [0, 1, 1]
+
+
+    elif(tangible.state == "place_confirm"):
+        tangible.button_msg.button = [2, 2, 0, 1, 0, 0]
+        tangible.button_msg.leds = [0, 0, 0]
+
+
+    elif(tangible.state == "basket"):
+        tangible.button_msg.button = [0, 0, 0, 0, 1, 0]
+        tangible.button_msg.leds = [0, 0, 0]
+
+
+    elif(tangible.state == "placing"):
+        tangible.button_msg.button = [1, 0, 0, 1, 0, 0]
+        tangible.button_msg.leds = [0, 0, 1]
 
 
--- tangible_communicate/scripts/tangible_communicate.py
+++ tangible_communicate/scripts/tangible_communicate.py
@@ -3,49 +3,74 @@
 import rospy
 import state_control
 from tangible_master.msg import button_leds
+import simple_ui
 
-recieved_msg = button_leds()
-
-def callback(data):
-    #Sets current button status to the one being published by the arduino
-    recieved_msg.button = data
-
-def talker():
-    """
-    ROS publisher/subscriber node, used to communicate with the tangible interface
-    via /button_led and /tangible_buttons
-    """
-
-    rospy.init_node('tangible_talker', anonymous=True)
-    rate = rospy.Rate(10)   #10 hz
-
-    #Pub
-    pub = rospy.Publisher('button_leds', button_leds, queue_size=10)
-
-    #Sub
-    rospy.Subscriber("tangible_buttons", button_leds, callback)
-
-    msg = button_leds()
-    msg.button = [0, 0, 0, 0, 0, 0]
-
-    state = "driving"
-
-    while not rospy.is_shutdown():
-        #TODO figure out what i named the serial pub and subscribe here
-        #Sets current state according to the array the buttons are publishing
-        state_control.set_state(state, recieved_msg.button)
-        #Compares state to required Led pattern and sets msg in accordance
-        state_control.compare_state(state, msg)
+class Tangible():
+    def __init__(self):
+        self.scooter = simple_ui.Scooter()
+        self.grasp = None
+        self.sample_points = None
+        self.center = None
 
 
-        rospy.loginfo(msg)
-        pub.publish(msg)
-        rate.sleep
+        self.state = "driving"
+
+        self.recieved_msg = button_leds()
+        self.send_msg = button_leds()
+
+        self.pickCloudGathered = False
+        self.basket_complete = False
+        self.placed_complete = False
+
+        #Michaels stuff dont know if i need ?
+        self.basket = False
+        self.place = False
+        self.getPCH = False
+        self.backLaser = False
+
+
+
+
+    def callback(self, data):
+        #Sets current button status to the one being published by the arduino
+        self.recieved_msg.button = data
+
+    def talker(self):
+        """
+        ROS publisher/subscriber node, used to communicate with the tangible interface
+        via /button_led and /tangible_buttons
+        """
+
+        rospy.init_node('tangible_talker', anonymous=True)
+
+        #Pub
+        pub = rospy.Publisher('button_leds', button_leds, queue_size=10)
+
+        #Sub
+        rospy.Subscriber("tangible_buttons", button_leds, self.callback)
+
+        self.send_msg.button = [0, 0, 0, 0, 0, 0]
+
+
+        while not rospy.is_shutdown():
+            #TODO figure out what i named the serial pub and subscribe here
+            #Sets current state according to the array the buttons are publishing
+            self.state_control.set_state(self)
+            #Compares state to required Led pattern and sets msg in accordance
+            state_control.compare_state(self)
+
+
+            rospy.loginfo(self.send_msg)
+            pub.publish(self.send_msg)
+            rospy.sleep(1)
 
 
 if __name__ == "__main__":
-	try:
-		talker()
+    try:
+        tangible = Tangible()
+        tangible.talker()
 
-	except rospy.ROSInterruptException:
-		pass
+    except rospy.ROSInterruptException:
+        pass
+
+
